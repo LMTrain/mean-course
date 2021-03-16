74bf6bc4743cb4851002c84b3fd114a37c313c39
Ltrain <layconmm@msn.com>
2021-03-15 21:02:53 -0500

    Add Express Framework

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+//CREATING A BACKEND SERVER WITH NODE JS
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//   res.end('HERE IS MY SERVER');
+// });
+
+// server.listen(process.env.PORT || 3000);
+
+
 const http = require('http');
+const app = require('./backend/app');
 
-const server = http.createServer((req, res) => {
-  res.end('HERE IS MY SERVER');
-});
+const port = process.env.PORT || 3000;
 
-server.listen(process.env.PORT || 3000);
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
