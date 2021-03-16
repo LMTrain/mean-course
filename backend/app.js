5c7bb29be708c2695fcd61d6a787c6c9e6287bd8
Ltrain <layconmm@msn.com>
2021-03-16 00:11:13 -0500

    Add Post to Backend Point

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+//ADDING HEADERS FOR CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader("Access-Control-Allow-Headers",
+  "Origin, X-requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+  "GET", "POST", "PATCH", "DELETE", "OPTIONS"
+  );
+  next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully!"
+  });
+});
 
 // app.use((req, res, next) => {
 //   console.log('First middleware');
 //   next();
 // });
 
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: 'fadf12421l',
