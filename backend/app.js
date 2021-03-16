4b7107e0734e1787ed4ec0181e0d001901a271d2
Ltrain <layconmm@msn.com>
2021-03-15 21:52:08 -0500

    Fetching initial post

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('First middleware');
+//   next();
+// });
 
-app.use((req, res, next) => {
-  res.send('Hello from Express');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fadf12421l',
+      title: 'First server-side post',
+      content: 'This is coming from the server side'
+    },
+    {
+      id: 'sadf32422l',
+      title: 'Second server-side post',
+      content: 'This is coming from the server side too'
+    }
+  ];
+res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
