32818646baccdf8468f3aed2b014fc545ef3ea72
Ltrain <layconmm@msn.com>
2021-03-18 21:27:28 -0500

    Connect Node Express to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require("'mongoose");
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://layconsm:layconsm@cluster0.3m10j.mongodb.net/<dbname>?retryWrites=true&w=majority");
+  then(() => {
+    console.log('CONNECTED TO DATABASE');
+  })
+  .catch(() => {
+    console.log('CONNECTION FAILED');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
--- backend/models/post.js
+++ backend/models/post.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
-const postSchema = mongoose.Schema({
-  title: { type: String, require: true},
-  content: {type: String, require: true}
+const postSchema = mongoose.Schema(
+  {
+  title: {
+    type: String,
+    require: true
+  },
+  content: {
+    type: String,
+    require: true}
 });
 
 module.exports = mongoose.model('Post', postSchema);
